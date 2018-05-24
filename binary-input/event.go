1673577f8ca275dbc8af90ddb9d956b91e695f9f
Evan Anderson <evan.k.anderson@gmail.com>
2018-05-24 12:40:27 -0700

    Move examples into subdirs
    Check in an example of using binary input & output
    
    Move structured input example to  a subdirectory to enable building both.

---
+++ binary-input/event.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/evankanderson/efgh"
+)
+
+// HandleEvent reads generic data, and returns an event with the data reversed.
+func HandleEvent(data []byte) ([]byte, error) {
+	log.Printf("Read event: %q. ", data)
+	for i, j := 0, len(data)-1; i < j; i, j = i+1, j-1 {
+		data[i], data[j] = data[j], data[i]
+	}
+	log.Printf("Reversed: %q\n", data)
+	return data, nil
+}
+
+func main() {
+	efgh.Start(HandleEvent)
+}
